5fc4fd997d6162cd1085d7eaeac63a8c77202d75
Joao Victor Lima <vctorlima.joao@gmail.com>
2023-10-04 10:03:13 -0300

    Resolucao do desafil DIO

---
+++ Resposta Desafio.sql
@@ -0,0 +1,89 @@
+--1
+SELECT 
+	Nome, Ano
+FROM Filmes
+
+--2
+SELECT 
+	Nome, Ano, Duracao
+FROM Filmes
+ORDER BY Nome
+
+--3
+SELECT 
+	Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+--4
+SELECT 
+	Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano = '1997'
+
+--5
+SELECT 
+	Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano LIKE '20%'
+
+--6
+SELECT 
+	Nome, Ano, Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC
+
+--7
+SELECT 
+	Ano, 
+	COUNT(*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+--8
+SELECT 
+	PrimeiroNome,
+	UltimoNome,
+	Genero
+FROM Atores
+WHERE Genero = 'M'
+
+--9
+SELECT 
+	PrimeiroNome,
+	UltimoNome,
+	Genero
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+--10
+SELECT 
+	Nome, Genero  
+FROM Filmes
+JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+--11
+SELECT 
+	Nome, Genero  
+FROM Filmes
+JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério'
+
+--12
+SELECT 
+	F.Nome, 
+	A.PrimeiroNome,
+	A.UltimoNome,
+	EF.Papel
+FROM Filmes F
+INNER JOIN ElencoFilme EF ON F.Id = EF.IdFilme
+INNER JOIN Atores A ON EF.IdAtor = A.Id
+
+
+
+
